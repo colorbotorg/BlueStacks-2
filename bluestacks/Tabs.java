959abc5bb46080b8de802257c4e9704e45c31a77
Trickster <zenziie@hotmail.com>
2016-11-22 15:09:25 +0100

    Update Tabs.java

--- bluestacks/Tabs.java
+++ bluestacks/Tabs.java
@@ -1,7 +1,6 @@
 package bluestacks;
 
 import com.zentek.colorbot.client.api.desktop.window.ChildWindow;
-import com.zentek.colorbot.client.api.desktop.window.ChildWindowNode;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,12 +15,11 @@ public final class Tabs {
 
     public String[] getTabs() {
         final List<String> tabs = new ArrayList<>();
-        final ChildWindowNode[] childeren = library.window.getChildren();
-        for (ChildWindowNode child : childeren) {
-            final ChildWindow entry = child.getData();
-            final String title = entry.getWindowTitle();
+        final ChildWindow[] childeren = library.window.getChildren();
+        for (ChildWindow child : childeren) {
+            final String title = child.getWindowTitle();
 
-            if (title != null && !title.equals("null") && entry.getClassName()
+            if (title != null && !title.equals("null") && child.getClassName()
                     .startsWith("WindowsForms10.Window.8.app.0.1ca0192")) {
                 tabs.add(title);
             }
